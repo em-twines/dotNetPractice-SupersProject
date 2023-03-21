d1f739b312b6be91c47799a9fbb5dc0c9e2fb5c8
em-twines <emdelbridge@gmail.com>
2023-03-21 19:38:47 -0400

    initial project created

---
+++ supersInCSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace supersInCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
