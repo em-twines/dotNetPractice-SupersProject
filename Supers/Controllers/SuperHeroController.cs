26c333035926460fdaaa79ada5f884df21f4bfcc
em-twines <emdelbridge@gmail.com>
2023-03-29 14:38:15 -0400

    created super controller and scaffolded views

---
+++ Supers/Controllers/SuperHeroController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Supers.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        // GET: SuperController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Supers/Models/SuperHero.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supers.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string CatchPhrase { get; set; }
+    }
+}
